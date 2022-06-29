9fdf180a48afb2507a8390ddb8134f9992124210
Rosa Turner <rmturner1220@gmail.com>
2022-06-29 09:22:44 -0400

    passed

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,8 +1,7 @@
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
-const potion = require('../lib/Potion');
+
 jest.mock('../lib/Potion.js');
-console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -11,6 +10,25 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)])
-    );
-  });                                                                   
+
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+  });            
+  
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
